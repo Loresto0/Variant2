a35b6308003418637cee7b62ab07ce0fa9d4840e
Loresto0 <awg7997@mail.ru>
2023-03-05 11:53:38 +0300

    gg

---
+++ Sean1/ConstructorApplication.cs
@@ -0,0 +1,22 @@
+﻿namespace Sean1;
+
+public class ConstructorApplication
+{
+    public string Surname { get; set; }
+    public string Name { get; set; }
+    public string Patronomyc { get; set; }
+    public string Number { get; set; }
+    public string Email { get; set; }
+    public string Organization { get; set; }
+    public string Post { get; set; }
+    public string Birthday { get; set; }
+    public string Seriapass { get; set; }
+    public string Numberpass { get; set; }
+    
+    
+    public DateTime DataLast { get; set; }
+    public DateTime DataFirst { get; set; }
+    public string Goal { get; set; }
+    public string Subdivision { get; set; }
+    public string FIOemployee { get; set; }
+}
---
+++ Sean1/Pages/AddSoloUser.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sean1.Pages.Shared;
+
+public class AddSoloUser : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+
+
+    [BindProperty]
+    public ConstructorApplication _app { get; set; }
+    
+    
+    public void OnPost()
+    {
+        
+        //в ссылке на апишку через $ указываю элементы которые хочу отправить, ниже пример
+        var test = _app.Birthday;
+        
+        
+        HttpClient client = new HttpClient();
+        var response = client.PostAsync("Тут должна быть ссылка на api, но аппишка не нету у меня", null);
+    }
+}
